34761949b29258639b81a59c45c76efcbe39cffe
TanunnasBK <31913677+jirutbk@users.noreply.github.com>
2023-09-24 22:09:57 +0700

    show table report

--- PTM_importData.py
+++ PTM_importData.py
@@ -1,4 +1,4 @@
-﻿import io, subprocess, csv
+﻿import os, io, subprocess, csv
 from PyPDF2 import PdfReader
 
 if input("ต้องการเปิดโฟลเดอร์ข้อมูลหรือไม่ (1:ใช่) : ") == "1":
@@ -12,8 +12,7 @@ while 1:
         print("ผิดพลาด : ต้องเป็นไฟล์ .pdf เท่านั้น !")
         continue        
     try:
-        reader = PdfReader(fileName)
-        #reader = PdfReader("sampleData.pdf")
+        reader = PdfReader(fileName)        
         break
     except FileNotFoundError:
         print("ผิดพลาด : ชื่อไฟล์ไม่ถูกต้อง !")
@@ -21,7 +20,8 @@ while 1:
 pageNum = len(reader.pages)
 header = ['Ref1', 'Name', 'Address', 'Province', 'Postcode']
 data = []
-print("-------------------------------------")
+os.system('cls')
+print("\033[3;37;42m   Ref1           ชื่อ - สกุล                        ที่อยู่                       อำเภอ/จังหวัด           รหัสไปรษณีย์ \033[0;37;40m")
 
 for page in reader.pages:    
     page_text = page.extract_text()    
@@ -35,27 +35,27 @@ for page in reader.pages:
             if line.find("Ref1") != -1:            
                 ref1 = line.split(": ")
                 ref1 = ref1[1].strip()
-                print(ref1)
+                print("{0:<15}".format(ref1), end="")
             elif line.find("นามสกุล :") != -1:
                 name = line.split(": ")
                 name = name[1].strip()
-                print(name)
+                print("{0:<35}".format(name[0:33]), end="")
             elif line.find("ที่อยู่ :") != -1:
                 address = line.split(": ")
                 address = address[1].strip()
-                print(address)
+                print("{0:<30}".format(address[0:28]), end="")
             elif line.find("อ.") != -1 and line.find("อ.") < 3:  #พบในตำแหน่งต้นๆ ของบรรทัด          
                 province = line.strip()
-                print(province)
+                print("{0:<30}".format(province[0:28]), end="")
             elif line.find("ไปรษณีย์ :") != -1:                
                 postcode = line.split(": ")
                 postcode = postcode[1].strip()
-                print(postcode) 
+                print("{0:<9}".format(postcode)) 
 
     row = [ref1, name, address, province, postcode]    
-    data.append(row)            
-    print("-------------------------------------")
+    data.append(row)          
 
+print("------------------------------------------------------------------------------------------------------------------------")
 print("รวมหนังสือแจ้ง จำนวน : ", pageNum)
 fileName = input("\nตั้งชื่อไฟล์ข้อมูลที่จะบันทึก (.csv) : ")
 
@@ -66,5 +66,5 @@ with open(fileName, 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(header)
     writer.writerows(data)
-        
+         
 input("\nบันทึกเรียบร้อย กดปุ่ม \"Enter\" เพื่อปิดโปรแกรม..")
