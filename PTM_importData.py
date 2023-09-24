bc6c47856379fb57d0f4a35e369a6a97b19e689b
TanunnasBK <31913677+jirutbk@users.noreply.github.com>
2023-09-24 12:39:12 +0700

    add exception check

--- PTM_importData.py
+++ PTM_importData.py
@@ -1,15 +1,26 @@
-﻿import io
-import csv
+﻿import io, subprocess, csv
 from PyPDF2 import PdfReader
 
-fileName = input("ป้อนชื่อไฟล์หนังสือแจ้งเตือน (.pdf) : ")
-reader = PdfReader(fileName)
-#reader = PdfReader("sampleData.pdf")
-
+while 1:
+    fileName = input("ป้อนชื่อไฟล์หนังสือแจ้งเตือน (.pdf) : ")
+    if fileName.find(".") == -1:
+        fileName = fileName + ".pdf"
+    elif fileName.split(".")[1] != "pdf":
+        print("ผิดพลาด : ต้องเป็นไฟล์ .pdf เท่านั้น !")
+        continue
+        
+    try:
+        reader = PdfReader(fileName)
+        #reader = PdfReader("sampleData.pdf")
+        break
+    except FileNotFoundError:
+        print("ผิดพลาด : ชื่อไฟล์ไม่ถูกต้อง !")
+        
 pageNum = len(reader.pages)
 
 header = ['Ref1', 'Name', 'Address', 'Province', 'Postcode']
 data = []
+print("-------------------------------------")
 
 for page in reader.pages:    
     page_text = page.extract_text()
@@ -52,9 +63,13 @@ print("รวมหนังสือแจ้ง จำนวน : ", pageNum)
 
 fileName = input("\nตั้งชื่อไฟล์ข้อมูลที่จะบันทึก (.csv) : ")
 
+if fileName.find(".") == -1:
+    fileName = fileName + ".csv"
+
 with open(fileName, 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(header)
     writer.writerows(data)
-
-input("\nบันทึกเรียบร้อย กดปุ่ม Enter เพื่อจบการทำงาน...")
+        
+if input("\nบันทึกเรียบร้อย ต้องการเปิดโฟลเดอร์ข้อมูลหรือไม่ (1:ใช่) : ") == "1":
+    subprocess.Popen(r'explorer /select,' + __file__)
